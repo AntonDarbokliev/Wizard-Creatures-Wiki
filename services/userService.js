006d513e6726c00c04f5284863b5e7bbde10a3b7
Anton <georgedarbokliev@gmail.com>
2023-10-07 16:49:30 +0300

    Login/register methods

---
+++ services/userService.js
@@ -0,0 +1,37 @@
+const User = require('../models/User.js')
+// const createToken = require("../utils/tokenHelper.js");
+const bcrypt = require("bcrypt");
+
+async function register(userData) {
+  const existing = await User.findOne({ email: userData.email });
+  if (existing) {
+    throw new Error("A user with this email already exists");
+  }
+
+  const createdUser = await User.create(userData);
+
+  const token = await createToken(createdUser);
+
+  return token;
+}
+
+async function login(userData) {
+  const user = await User.findOne({ email: userData.email });
+  if (!user) {
+    throw new Error("No user with this email has been found");
+  }
+  const correctPassword = await bcrypt.compare(userData.password, user.password);
+
+  if (!correctPassword) {
+    throw new Error("Wrong email or password");
+  }
+
+  const token = await createToken(user);
+
+  return token;
+}
+
+module.exports = {
+  register,
+  login,
+};
