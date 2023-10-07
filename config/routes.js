be5fd3edba1794cffff4aac81e5021c2e4ec85cf
Anton <georgedarbokliev@gmail.com>
2023-10-07 16:30:23 +0300

    Register/login pages

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const cookieParser = require("cookie-parser");
 const homeController = require("../controllers/homeController.js");
+const userController = require("../controllers/userController.js");
 module.exports = (app) => {
   app.use(cookieParser())
   app.use(homeController);
+  app.use("/user", userController);
+
 };
---
+++ controllers/userController.js
@@ -0,0 +1,68 @@
+// const { register, login } = require("../services/userService.js");
+// const { errorHelper } = require("../utils/errorHelpers.js");
+
+const userController = require("express").Router();
+
+userController.get("/register", async (req, res) => {
+  try {
+    res.render("register", {
+      title: "Register",
+    });
+  } catch (err) {
+    // const errors = errorHelper(err)
+    // res.render('register',{
+    //   title : 'Register',
+    //   errors
+    // })
+    console.log(err);
+  }
+});
+
+// userController.post("/register", async (req, res) => {
+//   try {
+//     const token = await register(req.body);
+//     res.cookie('auth',token,{httpOnly : true})
+//     res.redirect('/')
+//   } catch (err) {
+//     const errors = errorHelper(err)
+//     res.render('register',{
+//       title : 'Register',
+//       errors
+//     })
+//   }
+// });
+
+userController.get("/login", async (req, res) => {
+  try {
+    res.render("login", {
+      title: "Login",
+    });
+  } catch (err) {
+    // const errors = errorHelper(err)
+    // res.render('login',{
+    //   title : 'Login',
+    //   errors
+    // })
+  }
+});
+
+// userController.post("/login", async (req, res) => {
+//   try {
+//     const token = await login(req.body);
+//     res.cookie('auth',token,{httpOnly : true})
+//     res.redirect('/')
+//   } catch (err) {
+//     const errors = errorHelper(err)
+//     res.render('login',{
+//       title : 'Login',
+//       errors
+//     })
+//   }
+// });
+
+// userController.get("/logout", async (req, res) => {
+//   res.clearCookie('auth')
+//   res.redirect('/')
+// });
+
+module.exports = userController;
