695c9d8be1ed20648db1254b997e96631b29cbc1
Anton <georgedarbokliev@gmail.com>
2023-10-08 13:03:50 +0300

    Auth

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,10 @@
 const cookieParser = require("cookie-parser");
 const homeController = require("../controllers/homeController.js");
 const userController = require("../controllers/userController.js");
+const { auth } = require("../middlewares/authMiddleware.js");
 module.exports = (app) => {
   app.use(cookieParser())
+  app.use(auth)
   app.use(homeController);
   app.use("/user", userController);
 
---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,34 @@
+const SECRET_KEY = require("../config/secretKey.js");
+const { verify } = require("../utils/jwt.js");
+
+async function auth(req, res, next) {
+  const token = req.cookies["auth"];
+  if(token){
+      try {
+        const decodedToken = await verify(token, SECRET_KEY);
+    
+        req.user = decodedToken;
+        res.locals.isAuthenticated = true; // navigation
+        next();
+      } catch (err) {
+        res.clearCookie("auth");
+        res.redirect("/user/login");
+      }
+}else{
+    next()
+}
+}
+
+async function isAuthorized(req,res,next){
+    if(!req.user){
+        res.redirect('/user/login')
+    }
+    next()
+}
+
+
+
+module.exports = {
+  auth,
+  isAuthorized
+};
